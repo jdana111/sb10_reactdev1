e66cd3f7724c5dfe153f1fb927bf73e49ff5f8fb
John Dana <john@danasd.com>
2023-03-20 15:28:31 -0600

    Quiz 3 - Local variables don't work.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import { useState } from 'react';
+import { sculptureList } from './data.js';
 
-export default function Counter() {
-  const [number, setNumber] = useState(0);
+export default function Gallery() {
+  let index = 0;
 
+  function handleClick() {
+    index = index + 1;
+  }
+
+  let sculpture = sculptureList[index];
   return (
     <>
-      <h1>{number}</h1>
-      <button onClick={() => {
-        setNumber(number + 1);
-        setNumber(number + 1);
-        setNumber(number + 1);
-      }}>+3</button>
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+      <p>
+        {sculpture.description}
+      </p>
     </>
-  )
+  );
 }
