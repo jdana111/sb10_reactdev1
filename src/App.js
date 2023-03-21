6074ed708ee02e409a2276e134513a954962b713
John Dana <john@danasd.com>
2023-03-20 18:12:29 -0600

    Stop Propogation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+function Button({ onClick, children }) {
+  return (
+    <button onClick={e => {
+      e.stopPropagation();
+      onClick();
+    }}>
+      {children}
+    </button>
+  );
+}
+
 export default function Toolbar() {
   return (
     <div className="Toolbar" onClick={() => {
       alert('You clicked on the toolbar!');
     }}>
-      <button onClick={() => alert('Playing!')}>
+      <Button onClick={() => alert('Playing!')}>
         Play Movie
-      </button>
-      <button onClick={() => alert('Uploading!')}>
+      </Button>
+      <Button onClick={() => alert('Uploading!')}>
         Upload Image
-      </button>
+      </Button>
     </div>
   );
 }
