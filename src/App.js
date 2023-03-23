b742f916eddaa626dcc3f3cab8e8888870ed42e8
John Dana <john@danasd.com>
2023-03-23 10:19:03 -0600

    Splicing in objects to an array.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-let nextId = 0;
+let nextId = 3;
+const initialArtists = [
+  { id: 0, name: 'Marta Colvin Andrade' },
+  { id: 1, name: 'Lamidi Olonade Fakeye'},
+  { id: 2, name: 'Louise Nevelson'},
+];
 
 export default function List() {
-  const [name, setName] = useState("");
-  const [artists, setArtists] = useState([]);
+  const [name, setName] = useState('');
+  const [artists, setArtists] = useState(
+    initialArtists
+  );
+
+  function handleClick() {
+    const insertAt = 1; // Could be any index
+    const nextArtists = [
+      // Items before the insertion point:
+      ...artists.slice(0, insertAt),
+      // New item:
+      { id: nextId + 1, name },
+      // Items after the insertion point:
+      ...artists.slice(insertAt)
+    ];
+    console.log('nextId = ', nextId)
+    setArtists(nextArtists);
+    setName('');
+  }
 
   return (
     <>
       <h1>Inspiring sculptors:</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button
-        onClick={() => {
-          setArtists([...artists, { id: nextId++, name: name }]);
-        }}
-      >
-        Add
+      <input
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <button onClick={handleClick}>
+        Insert
       </button>
       <ul>
-        {artists.map((artist) => (
+        {artists.map(artist => (
           <li key={artist.id}>{artist.name}</li>
         ))}
       </ul>
