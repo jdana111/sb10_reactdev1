c63d85b38c9bc7152080bb5c1a6cc5b11c26720b
John Dana <john@danasd.com>
2023-03-27 11:49:20 -0600

    Deeply nested state versus not

--- src/App.js
+++ src/App.js
@@ -1,50 +1,33 @@
 import { useState } from 'react';
+import { initialTravelPlan } from './places.js';
 
-const initialItems = [
-  { title: 'pretzels', id: 0 },
-  { title: 'crispy seaweed', id: 1 },
-  { title: 'granola bar', id: 2 },
-];
-
-export default function Menu() {
-  const [items, setItems] = useState(initialItems);
-  const [selectedItem, setSelectedItem] = useState(
-    items[0]
+function PlaceTree({ place }) {
+  const childPlaces = place.childPlaces;
+  return (
+    <li>
+      {place.title}
+      {childPlaces.length > 0 && (
+        <ol>
+          {childPlaces.map(place => (
+            <PlaceTree key={place.id} place={place} />
+          ))}
+        </ol>
+      )}
+    </li>
   );
+}
 
-  function handleItemChange(id, e) {
-    setItems(items.map(item => {
-      if (item.id === id) {
-        return {
-          ...item,
-          title: e.target.value,
-        };
-      } else {
-        return item;
-      }
-    }));
-  }
-
+export default function TravelPlan() {
+  const [plan, setPlan] = useState(initialTravelPlan);
+  const planets = plan.childPlaces;
   return (
     <>
-      <h2>What's your travel snack?</h2> 
-      <ul>
-        {items.map((item, index) => (
-          <li key={item.id}>
-            <input
-              value={item.title}
-              onChange={e => {
-                handleItemChange(item.id, e)
-              }}
-            />
-            {' '}
-            <button onClick={() => {
-              setSelectedItem(item);
-            }}>Choose</button>
-          </li>
+      <h2>Places to visit</h2>
+      <ol>
+        {planets.map(place => (
+          <PlaceTree key={place.id} place={place} />
         ))}
-      </ul>
-      <p>You picked {selectedItem.title}.</p>
+      </ol>
     </>
   );
 }
