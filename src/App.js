1a24eb0b4c6a40ec58d116a51854f07bfc002c29
John Dana <john@danasd.com>
2023-03-28 13:45:38 -0600

    NATHO => Bad component.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+
+// NATHO =>
+// a) This component is wackadoodle. Here, we're returning the JSX and then we're trying to tweak it with gitElementById.
+// b) Is it ever legit to modify the DOM uysing document.getElementById('a').className = 'b'? 
+// c) It seems to me that I should always try to configure the JSX elements in the return expression using a variable or state variable. Your thoughts?
+// d) The next commit shows fix. 
 export default function Clock({ time }) {
   let hours = time.getHours();
   if (hours >= 0 && hours <= 6) {
