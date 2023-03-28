83732afb289abe0a70c3ee5c711ab478994e2cb3
John Dana <john@danasd.com>
2023-03-28 12:01:37 -0600

    NATHO => "Impure" component and StrictMode.

--- src/App.js
+++ src/App.js
@@ -1,131 +1,18 @@
-import { useState } from 'react';
+let guest = 0;
 
-function FilterableProductTable({ products }) {
-  const [filterText, setFilterText] = useState('');
-  const [inStockOnly, setInStockOnly] = useState(false);
-
-  return (
-    <div>
-      <SearchBar 
-        filterText={filterText} 
-        inStockOnly={inStockOnly} 
-        onFilterTextChange={setFilterText} 
-        onInStockOnlyChange={setInStockOnly} />
-      <ProductTable 
-        products={products} 
-        filterText={filterText}
-        inStockOnly={inStockOnly} />
-    </div>
-  );
+function Cup() {
+  // NATHO => I deliberately created this component based on an "impure function". Take a look at the output.
+  // What steps do I take to get <React.StrictMode> in index.js to catch this component?
+  guest = guest + 1;
+  return <h2>Tea cup for guest #{guest}</h2>;
 }
 
-function ProductCategoryRow({ category }) {
+export default function TeaSet() {
   return (
-    <tr>
-      <th colSpan="2">
-        {category}
-      </th>
-    </tr>
+    <>
+      <Cup />
+      <Cup />
+      <Cup />
+    </>
   );
-}
-
-function ProductRow({ product }) {
-  const name = product.stocked ? product.name :
-    <span style={{ color: 'red' }}>
-      {product.name}
-    </span>;
-
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{product.price}</td>
-    </tr>
-  );
-}
-
-function ProductTable({ products, filterText, inStockOnly }) {
-  const rows = [];
-  let lastCategory = null;
-
-  // NATHO => 
-  // a) Let's take a look at the products array of objects. 
-  // b) Play with the app in Chrome. (Specify a filterText value.)
-  // c) We're starting with an empty rows array and piling up new rows.
-  // d) Using the forEach / rows.push combo seems slick since it allows us to filter and create the category rows in one fell swoop. 
-  // e) I find kind of amazing that we're embedding <ProductCategoryRow /> and <ProductRow /> renders into individual row eleements of rows, 
-  //    we can just drop rows into <tbody>{rows}</tbody> AND IT WORKS. 
-  products.forEach((product) => {
-    if (
-      product.name.toLowerCase().indexOf(
-        filterText.toLowerCase()
-      ) === -1
-    ) {
-      return;
-    }
-    if (inStockOnly && !product.stocked) {
-      return;
-    }
-    if (product.category !== lastCategory) {
-      rows.push(
-        <ProductCategoryRow
-          category={product.category}
-          key={product.category} />
-      );
-    }
-    rows.push(
-      <ProductRow
-        product={product}
-        key={product.name} />
-    );
-    lastCategory = product.category;
-  });
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
-  );
-}
-
-function SearchBar({
-  filterText,
-  inStockOnly,
-  onFilterTextChange,
-  onInStockOnlyChange
-}) {
-  return (
-    <form>
-      <input 
-        type="text" 
-        value={filterText} placeholder="Search..." 
-        onChange={(e) => onFilterTextChange(e.target.value)} />
-      <label>
-        <input 
-          type="checkbox" 
-          checked={inStockOnly} 
-          onChange={(e) => onInStockOnlyChange(e.target.checked)} />
-        {' '}
-        Only show products in stock
-      </label>
-    </form>
-  );
-}
-
-const PRODUCTS = [
-  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
-];
-
-export default function App() {
-  return <FilterableProductTable products={PRODUCTS} />;
 }
