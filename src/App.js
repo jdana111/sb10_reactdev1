757239e0a6473b765afdda7c212a471709286990
John Dana <john@danasd.com>
2023-04-07 11:25:41 -0600

    More changes to UI tree

--- src/App.js
+++ src/App.js
@@ -2,26 +2,17 @@ import { useState } from 'react';
 
 export default function App() {
   const [isFancy, setIsFancy] = useState(false);
-  if (isFancy) {
-    return (
-      <div>
-        <Counter isFancy={true} />
-        <label>
-          <input
-            type="checkbox"
-            checked={isFancy}
-            onChange={e => {
-              setIsFancy(e.target.checked)
-            }}
-          />
-          Use fancy styling
-        </label>
-      </div>
-    );
-  }
   return (
     <div>
-      <Counter isFancy={false} />
+      {isFancy ? (
+        <div>
+          <Counter isFancy={true} /> 
+        </div>
+      ) : (
+        <section>
+          <Counter isFancy={false} />
+        </section>
+      )}
       <label>
         <input
           type="checkbox"
